1589d02efe2dd6d3f98eb0b76672462aff03dcdb
U-COMPUTE\gerald.updyke <krustwell@gmail.com>
2021-01-24 21:58:09 -0500

    Script to delete unused EBS volumes

--- delete_unused_ebs.sh
+++ delete_unused_ebs.sh
@@ -1,4 +1,4 @@
 for unused_volume in $(aws ec2 describe-volumes --filters Name=status,Values=available \
 --output text --query 'Volumes[*].Attachments[].{VolumeID:VolumeId}') ; 
-do echo aws ec2 delete-volume --volume-id $unused_volume ; done
+do aws ec2 delete-volume --volume-id $unused_volume ; done
 
